656c89da890ee3ff2fc350ac916b146517f3c4d2
alswnsp411 <alswnsp411@naver.com>
2025-03-29 23:20:31 +0900

    springboot jpa 세팅

---
+++ src/test/java/com/sk/skala/stockbackend/StockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.skala.stockbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
